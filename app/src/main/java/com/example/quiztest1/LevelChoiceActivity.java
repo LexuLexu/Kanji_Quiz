8722bac27ad87d816590f0e736bcf0811e68988a
LexuLexu <pepsiandram@gmail.com>
2020-06-30 15:08:51 +0100

    User requires name to do quiz

--- app/src/main/java/com/example/quiztest1/LevelChoiceActivity.java
+++ app/src/main/java/com/example/quiztest1/LevelChoiceActivity.java
@@ -2,15 +2,20 @@ package com.example.quiztest1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.logging.Level;
 
@@ -20,6 +25,20 @@ public class LevelChoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_choice);
+
+        CardView setNameCard = findViewById(R.id.setNameCard);
+        View noNameCover = findViewById(R.id.noNameCover);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+
+            String name = user.getDisplayName();
+
+            if (name.equals("")) {
+                setNameCard.setVisibility(View.VISIBLE);
+                noNameCover.setVisibility(View.VISIBLE);
+            }
+        }
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.BottomNavigationBar);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -60,20 +79,18 @@ public class LevelChoiceActivity extends AppCompatActivity {
     }
 
     public void go_to_n5English (View v) {
-
         startActivity(new Intent (getApplicationContext(), N5EnglishActivity.class));
-
     }
 
     public void go_to_n5Kana (View v) {
-
         startActivity(new Intent (getApplicationContext(), N5KanaActivity.class));
-
     }
 
+    public void go_to_profile (View v) {
+        startActivity(new Intent (getApplicationContext(), ProfileActivity.class));
+    }
     public void notAvailable (View v) {
-
         Toast.makeText(LevelChoiceActivity.this, "This level will be added in a future version", Toast.LENGTH_SHORT).show();
-
     }
+
 }
